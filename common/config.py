e2e75d3ed16cac8ac88586b47e20254e39c18b6a
tcollins2011 <tyler@nhlp.com>
2024-12-19 09:07:56 -0500

    restructure

---
+++ common/config.py
@@ -0,0 +1,9 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+PINECONE_API_KEY = os.getenv("PINECONE_API_KEY")
+PINECONE_ENV = os.getenv("PINECONE_ENV")
+PINECONE_INDEX = os.getenv("PINECONE_INDEX")
